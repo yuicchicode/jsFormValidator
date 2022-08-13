a4ec7c566538740bd61e831eee7a59fb083fab1a
yuicchicode <Yuicchisama@gmail.com>
2022-08-13 01:12:52 -0300

    Code Update

---
+++ script.js
@@ -0,0 +1,44 @@
+let yValidator = {
+    handleSubmit:(e) => {
+        e.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i=0;i<inputs.length;i++) {
+            let input = inputs [i];
+            let check = yValidator.checkInput(input);
+            if(check !== true) {
+                send = false;
+                console.log(check);
+            }
+        }
+
+        if(send) {
+            form.submit();
+        }
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value == '') {
+                            return 'campo não pode ser vazio.';
+                        }
+                    break;
+                    case 'min':
+                    break;    
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', yValidator.handleSubmit);
