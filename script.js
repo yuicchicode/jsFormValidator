403e3ab76c93ca76d571d4d9a2341966202ebca2
yuicchicode <Yuicchisama@gmail.com>
2022-09-10 19:56:54 -0300

    Update

--- script.js
+++ script.js
@@ -33,6 +33,17 @@ let yValidator = {
                         }
                     break;
                     case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return 'Necessário pelo menos '+rDetails[1]+'  caracteres';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail Invalido';
+                            }
+                        }
                     break;    
                 }
             }
